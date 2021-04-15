963e8476e70ab516b71393db5b8be8e549059a04
uqisdq <cheesyglaze@gmail.com>
2021-04-15 16:23:44 +0700

    Memindahkan import path package dan melakukan refactor

---
+++ config/static.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+    public : {
+        root: path.join(__dirname, '../public'),
+        prefix: '/',
+      },
+    assets : {
+        root: path.join(__dirname, '../public/assets'),
+        prefix: '/assets',
+        decorateReply: false
+      },
+    forms : {
+        root: path.join(__dirname, '../public/forms'),
+        prefix: '/forms',
+        decorateReply: false
+      },
+}
--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
-const path = require('path')
 
 // Register Plugins
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'public'),
-  prefix: '/',
-})
+fastify.register(require('fastify-static'), require("./config/static").public)
 
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'public/assets'),
-  prefix: '/assets',
-  decorateReply: false
-})
+fastify.register(require('fastify-static'), require("./config/static").assets)
 
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'public/forms'),
-  prefix: '/forms',
-  decorateReply: false
-})
+fastify.register(require('fastify-static'), require("./config/static").forms)
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
