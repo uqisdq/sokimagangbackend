dd57e444fcff290edcb1b6daccb9cd808264f85c
uqisdq <cheesyglaze@gmail.com>
2021-04-15 13:39:35 +0700

    awal hari kedua

---
+++ index.js
@@ -0,0 +1,23 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Declare a route
+fastify.get('/apaya', async (request, reply) => {
+  return { hello: 'apaya' }
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
