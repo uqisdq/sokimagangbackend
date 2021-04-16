4ab6d1998daa801800332e02d0cc371a9a0c0ea6
uqisdq <cheesyglaze@gmail.com>
2021-04-16 10:30:19 +0700

    menambahkan port

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ fastify.get('/apaya', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(5000)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
